0ada987bfda0c2416501cb14d93976e496ce6c62
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-10-03 13:38:04 +0300

    Add routing routes in app component and modify the css styling of the create component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
+import Create from "../pages/Create";
 import Home from "../pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -13,9 +13,9 @@ const Create = () => {
           <Input placeholder="End time/date" type="end" />
           <Input placeholder="Location" type="location" />
           <Input placeholder="Event photo" type="photo" />
-          <Button>Create</Button>
+          <Button position="left">Create</Button>
         </Form>
-        <Button>Next</Button>
+        <Button position="right">Next</Button>
       </Wrapper>
     </Container>
   );
@@ -23,14 +23,49 @@ const Create = () => {
 
 export default Create;
 
-const Container = styled.main``;
+const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f6f2ff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 300;
+  color: #240d57;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 0.625rem 0;
+  padding: 0.625rem;
+  &::placeholder {
+    color: #8456ec;
+  }
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  color: #fff;
+
+  background-image: linear-gradient(to right, #8456ec, #e87bf8);
+  width: 40%;
+
+  padding: 15px 20px;
+  cursor: pointer;
+  border: none;
+`;
