864ae0ea32c41373b5a202bd73b6c850c9f1f2af
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-10-03 11:26:55 +0300

    Modify the home page css styling to by dividing it into left and right section for the card and landing page information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Card from "../components/Card";
 
 const Home = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Left>
+        <Card />
+      </Left>
+      <Right>Right</Right>
+    </Container>
+  );
 };
 
 export default Home;
@@ -12,4 +20,15 @@ const Container = styled.main`
   height: 100vh;
   display: flex;
   background-color: #f6f2ff;
+  align-items: center;
+  padding: 3.75rem 6.25rem;
+  box-sizing: border-box;
+`;
+
+const Left = styled.section`
+  flex: 1;
+`;
+
+const Right = styled.section`
+  flex: 1;
 `;
