f8b1d22dbd347afafeae436e0d5077d6035f1e86
Maurice Nganga <61679467+moryno@users.noreply.github.com>
2022-10-03 10:55:28 +0300

    Create a home page that will be the landing page of the site

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
+import Home from "../pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
+    <div>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return <Container></Container>;
+};
+
+export default Home;
+
+const Container = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: #f6f2ff;
+`;
